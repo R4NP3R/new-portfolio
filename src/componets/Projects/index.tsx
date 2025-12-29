19da3c01fb176999508ecbe22314e989ecb79865
R4NP3R <ssrafa.2014@gmail.com>
2025-12-28 21:37:57 -0300

    refactor: adiciona um projeto moviflix no portfolio

--- src/componets/Projects/index.tsx
+++ src/componets/Projects/index.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import PackageIcon from "../../assets/package_dark.svg?react";
-import ModuleIcon from "../../assets/module_dark.svg?react";
 import { SlArrowDown, SlArrowRight } from "react-icons/sl";
+import ModuleIcon from "../../assets/module_dark.svg?react";
+import PackageIcon from "../../assets/package_dark.svg?react";
 import { ProjectInfo, type ProjectInfoData } from "../../ProjectInfo";
+import moviefliximg from "../../assets/BE-movieflix.png"
 
 interface ProjectTecnologies {
   name: string;
@@ -33,6 +34,7 @@ const projetosFrontEnd: ProjectInfoData[] = [
           {
             name: "framework",
             data: "react",
+            isFinal: true
           },
         ],
       },
@@ -62,7 +64,7 @@ const projetosFrontEnd: ProjectInfoData[] = [
           {
             name: "framework",
             data: "react",
-            isFinal: true,
+            isFinal: true
           },
         ],
       },
@@ -173,7 +175,34 @@ const projetosFrontEnd: ProjectInfoData[] = [
   },
 ];
 
-// const projetosBackEnd: ProjectInfoData[] = [
+const projetosBackEnd: ProjectInfoData[] = [
+  {
+    annotation: [
+      {
+        name: "Stacks",
+        parameters: [
+          {
+            name: "language",
+            data: "java",
+          },
+          {
+            name: "framework",
+            data: "sprigboot",
+            isFinal: true
+          },
+        ],
+      },
+    ],
+    image: moviefliximg,
+    name: "MovieFlix",
+    projectLinks: {
+      github: {
+        link: "https://github.com/R4NP3R/movieFlix",
+        name: "movieflix",
+      },
+    },
+  },
+];
 //   {
 //     annotation: [
 //       {
@@ -267,15 +296,9 @@ export const Projects = () => {
       )}
       {isActive === "backEnd" && (
         <div className="w-full bg-linear-to-b from-[#1E1F22] from-95% to-[#0D1117] grid grid-cols-2 gap-6 px-4 py-1">
-          <div>
-            <span className="text-[#F96967]">
-              Exception in thread "BackEnd" java.lang.NullPointerException
-            </span>
-            <br />
-            <span className="text-[#F96967] ml-5">
-              at life.projects.backend{"(null)"}
-            </span>
-          </div>
+          {projetosBackEnd.map((projeto) => (
+            <ProjectInfo projects={projeto} />
+          ))}
         </div>
       )}
       {isActive === "fullStack" && (
